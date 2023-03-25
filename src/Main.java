8161a721e6116750e1e472ad993737a2b584473e
Nikita Mayorov <MayorovNikita7@yandex.ru>
2023-03-25 13:03:29 +0500

    Конечная версия

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите кол-во менеджеров:");
+        int[] number = new int [scanner.nextInt()];
+
+        for (int x = 0; x < number.length; x++){
+            System.out.println("Введите кол-во продаж менеджера: ");
+            number[x] = scanner.nextInt();
+        }
+        SalesManager salesManager =new SalesManager(number);
+        System.out.println("Максимальное кол-во продаж: " + salesManager.max());
+    }
+}
